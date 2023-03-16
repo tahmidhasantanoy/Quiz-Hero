02f288df172e1a81e0ac8baa2a9efdcc063ab771
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-03-16 13:02:20 +0600

    Previous exam result stored successfully

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ startQuiz.addEventListener("click", () => {
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
   const data = await res.json();
+  console.log(data);
   quizData = data;
   displayQuiz(data);
 };
@@ -76,16 +77,21 @@ const displayQuiz = (data) => {
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
+  console.log("submit clicked");
+
   if (answers.length < 6) {
     return;
   }
-  quizTimer(true);
+
+  quizTimer(true); //not defined maybe
   answersContainer.innerHTML = `<div class="my-4">
   <i class="fa-solid fa-fan animate-spin text-2xl text-green-600"></i>
   <p class="text-xs animate-pulse">Please Wait, We are checking...</p>
-</div>`;
+</div>`; //like spinner
+
   let timeTaken = document.querySelector("#count");
   let totalMark = 0;
+
   let grade = {
     status: "",
     color: "",
@@ -109,7 +115,12 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results")); //prev
+  // let savedStorage = getLocalStorageItems(); //last
+  // console.log(savedStorage); // getLocalStorageItems() working
+
+
+
   if (storage) {
     localStorage.setItem(
       "results",
@@ -138,32 +149,40 @@ document.querySelector("#submit").addEventListener("click", () => {
   // Right side bar/ answer section
   let x = setTimeout(() => {
     showAnswers(answers);
-    displayResult.innerHTML = `<div
-    class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]"
-  >
-    <h3 class="text-xl ${grade.color}">${grade.status}</h3>
-    <h1 class="text-3xl font-bold my-2">
-      ${totalMark}<span class="text-slate-800">/60</span>
-    </h1>
-    <p class="text-sm flex justify-center items-center gap-2">
-      Total Time: <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
-        "sec",
-        ""
-      )}<span class="text-xs">sec</span></span>
-    </p>
-  </div>
-  
+    displayResult.innerHTML = 
+    `<div
+         class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]"
+         >
+       <h3 class="text-xl ${grade.color}">${grade.status}</h3>
+       <h1 class="text-3xl font-bold my-2">
+         ${totalMark}<span class="text-slate-800">/60</span>
+       </h1>
+       <p class="text-sm flex justify-center items-center gap-2">
+         Total Time: 
+         <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
+           "sec",
+           ""
+         )}
+         <span class="text-xs">sec</span></span>
+       </p>
+       <div id="prev-result">  </div>
+     </div>
+
+
   <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
+
+
   ${
     storage
-      ? `<div class="mt-5">
-      <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
-    <div
-    class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
-    <div>Marks</div>
-    <div>Grade</div>
-    <div>Time</div>
-    </div>
+       ?`<div class="mt-5">
+       <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
+     <div
+     class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
+     <div>Marks</div>
+     <div>Grade</div>
+     <div>Time</div>
+     </div>
+
     ${storage
       ?.reverse()
       ?.map(
@@ -180,7 +199,19 @@ document.querySelector("#submit").addEventListener("click", () => {
   </div>
   `;
 
+
     clearTimeout(x);
   }, 1500);
   window.scrollTo(0, 0);
 });
+
+//Extra code for getting element
+const getLocalStorageItems = () => {
+  let itemsArray = [];
+  const savedResult = localStorage.getItem("results");
+  if (savedResult) {
+    itemsArray = JSON.parse(savedResult);
+  }
+  return itemsArray;
+};
+//Extra code for getting element
